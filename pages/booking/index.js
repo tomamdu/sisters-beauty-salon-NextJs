8742ac7eb9a2281357fecfd2516c22b1e2edc006
tomamdu <tomamdu@users.noreply.github.com>
2021-11-12 01:11:04 +0300

    booking

--- pages/booking/index.js
+++ pages/booking/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import Script from 'next/script'
 
 
 import Header from '../../components/landingPage/header'
@@ -16,13 +16,13 @@ export default function Booking() {
        <Head>
        <title>Sisters Beauty Salon Booking</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-        <meta name="description" content="Sisters beauty salon is an establishment that offers a variety of cosmetic treatments and cosmetic services for women in Addis Ababa,Ethiopia. Beauty salons  offer a variety of services including professional hair cutting and styling, manicures and pedicures, and often cosmetics, makeup and makeovers."/>     
+      <meta name="description" content="Sisters beauty salon is an establishment that offers a variety of cosmetic treatments and cosmetic services for women in Addis Ababa,Ethiopia. Beauty salons  offer a variety of services including professional hair cutting and styling, manicures and pedicures, and often cosmetics, makeup and makeovers."/>     
 
-      <script type="text/javascript" src="/js/jquery-3.5.1.min.js"></script>
-      <script type="text/javascript" src="/js/bootstrap.bundle.min.js"></script>
-      <script type="text/javascript" src="/vendor/owl-carousel/js/owl.carousel.min.js"></script>
-      <script type="text/javascript" src="/vendor/wow/wow.min.js"></script>
-      <script type="text/javascript" src="/js/theme.js"></script>
+      <Script type="text/javascript" src="/js/jquery-3.5.1.min.js"></Script>
+      <Script type="text/javascript" src="/js/bootstrap.bundle.min.js"></Script>
+      <Script type="text/javascript" src="/vendor/owl-carousel/js/owl.carousel.min.js"></Script>
+      <Script type="text/javascript" src="/vendor/wow/wow.min.js"></Script>
+      <Script type="text/javascript" src="/js/theme.js"></Script>
         
 
       </Head> 
