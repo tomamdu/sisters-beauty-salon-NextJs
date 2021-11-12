6f00649c8317c9694c299e5639e6313f916fc403
tomamdu <tomamdu@users.noreply.github.com>
2021-11-12 03:59:10 +0300

    script

--- components/admin/layout/index.js
+++ components/admin/layout/index.js
@@ -31,11 +31,12 @@ export default function Layout({ children }) {
             <meta charSet="utf-8" />           
             <title>Sisters Admin</title>             
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"></link>
-            <Script async src="https://kit.fontawesome.com/53891a1ef0.js" crossOrigin="anonymous"></Script>
+            
             <link rel="preconnect" href="https://fonts.googleapis.com"></link>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
            <link href="https://fonts.googleapis.com/css2?family=Mohave:wght@300;400&family=Open+Sans:wght@300&display=swap" rel="stylesheet"></link>
-            </Head>
+      </Head>
+      <Script async src="https://kit.fontawesome.com/53891a1ef0.js" crossOrigin="anonymous"></Script>
       <Header/>
       <Side role = {role} name = {name} email = {email}/>
       <div className="body container ">
